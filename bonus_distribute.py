b83be6dc2b74882038c43cd2e26d2e8c6b5aa45a
wiedld <dlw405@gmail.com>
2015-05-08 18:11:01 -0700

    read data input, base function to return users with significance

---
+++ bonus_distribute.py
@@ -0,0 +1,49 @@
+
+
+
+def retrieve_data(url):
+    from urllib2 import urlopen
+
+    response = urlopen(url)
+    result = response.read()
+
+    return eval(result)         # result is a list.  items are users.
+
+
+
+def determine_num_following(result):
+    print len(result)
+
+    num_following_user = {}
+    for user in result:
+        who_tracking = user["tracks"]       # will return a list
+
+        for tracked in who_tracking:
+            if tracked not in num_following_user:
+                num_following_user[tracked] = 1
+            else:
+                num_following_user[tracked] += 1
+
+    return num_following_user
+
+
+
+def determine_max(adict):
+    """takes a list of items, with values = int.
+    returns a list of top items above 3 std threshold"""
+    import numpy
+
+    # list of numbers, of followers
+    num_followers = adict.values()
+    f_stats = numpy.array(num_followers)
+
+    avg, stdev = f_stats.mean(), f_stats.std()
+    threshold = avg + (3*stdev)
+
+    return [k for k,v in adict.items() if v >= threshold]
+
+
+
+data = retrieve_data("https://keybase.io/jobs/q/b5c602f8306d711b?page=data")
+num_following_user = determine_num_following(data)
+print determine_max(num_following_user)
