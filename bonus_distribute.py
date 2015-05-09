f0b5c84051e2da856af469c1e62182c1bd928c6f
wiedld <dlw405@gmail.com>
2015-05-08 18:37:44 -0700

    determining num users in common btwn selection criteria

--- bonus_distribute.py
+++ bonus_distribute.py
@@ -27,7 +27,6 @@ def determine_num_following(result):
 
 
 def make_list_account_types(result):
-    print len(result)
 
     acct_types = {}
     for user in result:
@@ -43,22 +42,18 @@ def make_list_account_types(result):
 
 
 def determine_num_dev_accounts(result):
-    print len(result)
 
-    num_following_user = {}
-    for user in result:
-        who_tracking = user["tracks"]       # will return a list
+    num_accts = {}
+    for user in result:            # each user is a dict of info
+        accts = user.keys()       # will return a list
 
-        for tracked in who_tracking:
-            if tracked not in num_following_user:
-                num_following_user[tracked] = 1
-            else:
-                num_following_user[tracked] += 1
+        user_id = user["keybase"]
+        num_accts[user_id] = len(accts)
 
-    return num_following_user
+    return num_accts
 
 
-def determine_max(adict):
+def determine_max(adict,k):
     """takes a list of items, with values = int.
     returns a list of top items above 3 std threshold"""
     import numpy
@@ -68,7 +63,7 @@ def determine_max(adict):
     f_stats = numpy.array(num_followers)
 
     avg, stdev = f_stats.mean(), f_stats.std()
-    threshold = avg + (3*stdev)
+    threshold = avg + (k*stdev)
 
     return [k for k,v in adict.items() if v >= threshold]
 
@@ -80,7 +75,19 @@ data = retrieve_data("https://keybase.io/jobs/q/b5c602f8306d711b?page=data")
 
 # under the assumption that the most followed are influencers
 num_following_user = determine_num_following(data)
-List_most_followed = determine_max(num_following_user)
+list_most_followed = determine_max(num_following_user,3)
+print len(list_most_followed)   #121
 
 # under the assumption that we want to reward those with more accounts
-print make_list_account_types(data)
+# print make_list_account_types(data)
+##  {'reddit': 6534, 'github': 25687, 'twitter': 24997,
+# 'keybase': 44739, 'tracks': 44739, 'hackernews': 2866, 'pgp': 37128}
+num_accts_ea_user = determine_num_dev_accounts(data)
+most_accts = determine_max(num_accts_ea_user, 2)
+print len(most_accts) # 1420
+
+# num of those in common between two selection criteria so far
+print len([user for user in most_accts if user in list_most_followed])  #16
+
+# most number on github
+#  https://developer.github.com/v3/users/followers/
