6d159f9b665f39a3992d050ed370a7e8d9def271
wiedld <dlw405@gmail.com>
2015-05-08 18:18:14 -0700

    read data input, base function to return users with significance

--- bonus_distribute.py
+++ bonus_distribute.py
@@ -10,7 +10,6 @@ def retrieve_data(url):
     return eval(result)         # result is a list.  items are users.
 
 
-
 def determine_num_following(result):
     print len(result)
 
@@ -26,6 +25,37 @@ def determine_num_following(result):
 
     return num_following_user
 
+
+def make_list_account_types(result):
+    print len(result)
+
+    acct_types = {}
+    for user in result:
+        accts = user.keys()       # will return a list
+
+        for acct in accts:
+            if acct not in acct_types:
+                acct_types[acct] = 1
+            else:
+                acct_types[acct] += 1
+
+    return acct_types
+
+
+def determine_num_dev_accounts(result):
+    print len(result)
+
+    num_following_user = {}
+    for user in result:
+        who_tracking = user["tracks"]       # will return a list
+
+        for tracked in who_tracking:
+            if tracked not in num_following_user:
+                num_following_user[tracked] = 1
+            else:
+                num_following_user[tracked] += 1
+
+    return num_following_user
 
 
 def determine_max(adict):
@@ -43,7 +73,14 @@ def determine_max(adict):
     return [k for k,v in adict.items() if v >= threshold]
 
 
+################################################################
+
 
 data = retrieve_data("https://keybase.io/jobs/q/b5c602f8306d711b?page=data")
+
+# under the assumption that the most followed are influencers
 num_following_user = determine_num_following(data)
-print determine_max(num_following_user)
+List_most_followed = determine_max(num_following_user)
+
+# under the assumption that we want to reward those with more accounts
+print make_list_account_types(data)
