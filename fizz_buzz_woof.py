67352431c38252192afe1f06b5b466cb1aa71161
wiedld <dlw405@gmail.com>
2015-05-25 21:43:45 -0700

    time optimized, much faster

--- fizz_buzz_woof.py
+++ fizz_buzz_woof.py
@@ -1,4 +1,5 @@
 
+
 def solution(N):
     """given N.  Print out 1->N, but replacing ints based on the rules:
         if divisible by 3 -> Fizz
@@ -11,24 +12,35 @@ def solution(N):
     # 35 is 5*7
     # 105 is 3*5*7
 
-    for num in range(1,N+1):
-        if num%105 == 0:
-            print "FizzBuzzWoof"
-        elif num%35 == 0:
-            print "BuzzWoof"
-        elif num%21 == 0:
-            print "FizzWoof"
-        elif num%15 == 0:
-            print "FizzBuzz"
-        elif num%7 == 0:
-            print "Woof"
-        elif num%5 == 0:
-            print "Buzz"
-        elif num%3 == 0:
-            print "Fizz"
-        else:
-            print num
+    # make list of nums
+    results = [x for x in range (1,N+1)]
+
+    # replace for multiples of variables
+    results = word_replace(results, 3, "Fizz")
+    results = word_replace(results, 5, "Buzz")
+    results = word_replace(results, 7, "Woof")
+    results = word_replace(results, 21, "FizzWoof")
+    results = word_replace(results, 35, "BuzzWoof")
+    results = word_replace(results, 105, "FizzBuzzWoof")
+
+    # print out result
+    for item in results:
+        print item
+
+
+def word_replace(num_list, factorial, word):
+    multiple = 1
+
+    # replace at indexed location
+    while (multiple*factorial) <= len(num_list):
+        index = (multiple * factorial)-1    # list is 1->N
+        num_list[index] = word
+        multiple += 1
+
+    return num_list
+
 
 solution(24)
-
 solution(105)
+solution(2000000)
+
